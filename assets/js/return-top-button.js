73377e18e825d87e0d9e95d636a403f10fb8fd21
Crange0114 <151908073+Crange0114@users.noreply.github.com>
2023-12-06 16:07:41 +0800

    Create return-top-button.js

---
+++ assets/js/return-top-button.js
@@ -0,0 +1,4 @@
+document.getElementById("return-top-button").addEventListener("click", function() {
+  document.body.scrollTop = 0; // For Safari
+  document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
+});
