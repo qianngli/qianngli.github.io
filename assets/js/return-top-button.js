0243c2e92151e7f871c338e42f63c931bfb969b3
Crange0114 <151908073+Crange0114@users.noreply.github.com>
2023-12-06 16:17:45 +0800

    Update return-top-button.js

--- assets/js/return-top-button.js
+++ assets/js/return-top-button.js
@@ -1,3 +1,15 @@
+window.addEventListener("scroll", function() {
+  var returnTopButton = document.getElementById("return-top-button");
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) 
+  {
+    returnTopButton.style.display = "block";
+  } 
+  else
+  {
+    returnTopButton.style.display = "none";
+  }
+});
+
 document.getElementById("return-top-button").addEventListener("click", function() {
   document.body.scrollTop = 0; // For Safari
   document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE, and Opera
